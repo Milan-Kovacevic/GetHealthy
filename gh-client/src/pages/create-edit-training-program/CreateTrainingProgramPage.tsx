75349b8dc8480b6dd90293f1a8c1c428579298e5
Ana Djurdjevic <93453858+AnaDjurdjevic@users.noreply.github.com>
2024-11-18 16:53:39 +0100

    font

--- gh-client/src/pages/create-edit-training-program/CreateTrainingProgramPage.tsx
+++ gh-client/src/pages/create-edit-training-program/CreateTrainingProgramPage.tsx
@@ -4,7 +4,7 @@ import ExercisePlanBuilder from "./components/ExercisePlanBuilder";
 const CreateTrainingProgramPage = () => {
   return (
     <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Create Training Program</h1>
+      <h1 className="text-3xl font-bold mb-4">Create Training Program</h1>
       <GeneralInformationForm isEdit={false} />
       <ExercisePlanBuilder />
     </div>
