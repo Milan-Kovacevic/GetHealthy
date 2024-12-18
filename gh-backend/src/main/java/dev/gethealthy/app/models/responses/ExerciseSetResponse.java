ad079799de05190f92e2583b98764132d9b1e972
Sasa Mikic <sale9002@gmail.com>
2024-12-18 18:33:14 +0100

    exercise set

---
+++ gh-backend/src/main/java/dev/gethealthy/app/models/responses/ExerciseSetResponse.java
@@ -0,0 +1,21 @@
+package dev.gethealthy.app.models.responses;
+
+import dev.gethealthy.app.repositories.MetricsRepository;
+import lombok.Data;
+
+@Data
+public class ExerciseResponse {
+    private Integer id;
+
+    private String exerciseName;
+
+    private String description;
+
+    private String videoLink;
+
+    private Integer userId;
+
+    private MetricsResponse firstExerciseMetric;
+
+    private MetricsResponse secondExerciseMetric;
+}
